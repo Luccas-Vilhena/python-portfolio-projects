25b0f0fd37b44ad7bb48ad83546c0a53f197d4e1
Luccas <lucas7.lvilhena@gmail.com>
2026-09-02 01:39:25 -0300

    Add files via upload

---
+++ contact-book/Planner 4.0.py
@@ -0,0 +1,37 @@
+import sys
+
+from Crud.Arquivo import criar_arquivo
+from Crud.Interface import menu, cabecalho, cor, pausar
+from Crud.Contato import adicionar_contato, ver_contatos, excluir_contato
+
+criar_arquivo('agenda.txt')
+opcoes = ['Adicionar Contato', 'Ver Contatos', 'Deletar Contato', 'Sair']
+
+while True:
+    cabecalho(cor('Menu Principal'.center(30), 36))
+    menu(opcoes)
+
+    while True:
+        try:
+            opcao = int(input('Digite o que deseja fazer: ').strip())
+            if 1 <= opcao <= len(opcoes):
+                break
+            else:
+                print(cor('ERRO, Digite apenas uma das opções válida!', 31))
+        except ValueError:
+            print(cor('ERRO, Digite apenas números!', 31))
+        except KeyboardInterrupt:
+            print(cor('Encerrando programa...',31))
+            sys.exit()
+
+    if opcao == 1:
+        adicionar_contato()
+    elif opcao == 2:
+        ver_contatos()
+    elif opcao == 3:
+        excluir_contato()
+    elif opcao == 4:
+        print(cor('Até logo!', 36))
+        break
+
+    pausar()
